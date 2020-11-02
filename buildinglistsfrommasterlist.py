4f26241f66c7946fdffedeeca4ec51c48c8e717e
Henry Tjhia <h.k.tjhia@gmail.com>
2020-11-03 00:02:01 +0700

    Update buildinglistsfrommasterlist.py

--- buildinglistsfrommasterlist.py
+++ buildinglistsfrommasterlist.py
@@ -1,7 +1,3 @@
-You count the elements in your source list. You count the elements in your test list(s).
-
-If your source list has enough elements (more or equal occurences of that number) then your test-list has, you can construct it from the source list:
-
 from collections import Counter
 from itertools import chain
 
